60cc16733333480cfdf5e7b715065a83000ed806
Wade <wadechen@outlook.com>
2021-10-14 14:36:53 +0800

    first commit

---
+++ src/main/java/com/cn/cellx/gatewaymiddleware/GatewaymiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.cn.cellx.gatewaymiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaymiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaymiddlewareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cn/cellx/gatewaymiddleware/GatewaymiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.cellx.gatewaymiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaymiddlewareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
