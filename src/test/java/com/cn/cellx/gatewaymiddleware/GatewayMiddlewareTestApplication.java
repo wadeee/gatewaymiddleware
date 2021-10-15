584da8835b2ea8d0cc3212d11640419d16d07de4
Wade <wadechen@outlook.com>
2021-10-15 11:42:36 +0800

    delete useless comment

--- src/test/java/com/cn/cellx/gatewaymiddleware/GatewayMiddlewareTestApplication.java
+++ src/test/java/com/cn/cellx/gatewaymiddleware/GatewayMiddlewareTestApplication.java
@@ -1,13 +1,11 @@
 package com.cn.cellx.gatewaymiddleware;
 
 import com.cn.cellx.gatewaymiddleware.NIOClient.NIOClient;
-import com.cn.cellx.gatewaymiddleware.NIOServer.NIOServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-import java.net.InetSocketAddress;
 
 @SpringBootApplication
 public class GatewayMiddlewareTestApplication {
